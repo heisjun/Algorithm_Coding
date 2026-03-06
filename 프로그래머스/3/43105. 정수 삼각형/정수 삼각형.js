11124c1738e8f0ee7eca1427787cabebefe5240a
JunHyuck Kim <82956755+heisjun@users.noreply.github.com>
2026-03-06 18:02:21 +0900

    [level 3] Title: 정수 삼각형, Time: 5.43 ms, Memory: 41.9 MB -BaekjoonHub

---
+++ 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.js
@@ -0,0 +1,24 @@
+function solution(triangle) {
+
+    for (let i = 1; i < triangle.length; i++) {
+   
+        for (let j = 0; j < triangle[i].length; j++) {
+            
+       
+            if (j === 0) {
+                triangle[i][j] += triangle[i - 1][0];
+            } 
+         
+            else if (j === i) {
+                triangle[i][j] += triangle[i - 1][j - 1];
+            } 
+      
+            else {
+                triangle[i][j] += Math.max(triangle[i - 1][j - 1], triangle[i - 1][j]);
+            }
+        }
+    }
+    
+    const lastRow = triangle[triangle.length - 1];
+    return Math.max(...lastRow);
+}
