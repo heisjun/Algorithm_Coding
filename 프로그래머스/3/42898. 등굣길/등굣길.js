39fa7552d8431bbb1f2b73f5a41905416f9fac83
JunHyuck Kim <82956755+heisjun@users.noreply.github.com>
2026-03-30 23:50:19 +0900

    [level 3] Title: 등굣길, Time: 1.90 ms, Memory: 33 MB -BaekjoonHub

--- 프로그래머스/3/42898. 등굣길/등굣길.js
+++ 프로그래머스/3/42898. 등굣길/등굣길.js
@@ -1,20 +1,30 @@
 function solution(m, n, puddles) {
-    const map = Array.from((new Array(n+1)),()=> new Array(m+1).fill(0));
-    map[1][1] =1
-    
-    for(let i =0; i < puddles.length; i++){
-        map[puddles[i][1]][puddles[i][0]] = 'x'
+    var shortest = Array.from({ length: n }, () =>
+        Array.from({ length: m }, () => 0)
+    );
+    shortest[0][0] = 1;
+
+    for (const [x, y] of puddles) {
+        shortest[y - 1][x - 1] = -1;
     }
-    
-      for(let i = 1; i < map.length; i++){
-        for(let j =1; j<map[i].length; j++){
-            if(i ===1 && j ===1) continue;
-            if(map[i][j] === 'x') continue;          
-            let n = (map[i][j-1] === 'x'? 0 :map[i][j-1])  + (map[i-1][j] ==='x'? 0 : map[i-1][j])
-            n = n % 1000000007;
-             map[i][j] = n
+
+    for (let i = 0; i < n; i++) {
+        for (let j = 0; j < m; j++) {
+            if (i === 0 && j === 0) continue;
+            if (shortest[i][j] === -1) continue;
+
+            var top = 0;
+            if (i - 1 >= 0 && shortest[i - 1][j] !== -1) {
+                top = shortest[i - 1][j] % 1000000007;
+            }
+            var left = 0;
+            if (j - 1 >= 0 && shortest[i][j - 1] !== -1) {
+                left = shortest[i][j - 1] % 1000000007;
+            }
+
+            shortest[i][j] = top + left;
         }
     }
-    return map[n][m] % 1000000007
-   
+
+    return shortest[n - 1][m - 1] % 1000000007;
 }
